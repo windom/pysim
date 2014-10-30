4d93f8516cff3126877190a4ab542015dad7af87
windom <miklosgabor86@gmail.com>
2014-10-30 08:41:18 +0200

    Experimental: tracking 2 - not working

--- test2.py
+++ test2.py
@@ -1,35 +1,73 @@
+import types
 
-class PropertyDictWrapper:
-    def __init__(self, owning_prop, owning_instance, the_dict):
+
+def install(target, owning_prop, owning_instance, newprops):
+    print("Installing", target)
+    if isinstance(target, dict) or isinstance(target, list):
+        return PropertyCollectionWrapper(owning_prop, owning_instance, target)
+    elif isinstance(target, PropertyBase):
+        def _(self, prop, old_value, new_value):
+            owning_prop.changed(newprops + prop, owning_instance, old_value, new_value)
+
+        target.__propchange__ = types.MethodType(_, target)
+        return target
+    else:
+        return target
+
+
+class PropertyCollectionWrapper:
+    def __init__(self, owning_prop, owning_instance, wrapped):
         self.owning_prop = owning_prop
         self.owning_instance = owning_instance
-        self.the_dict = the_dict
+        self.wrapped = wrapped
+
+        if isinstance(wrapped, dict):
+            for k,v in wrapped.items():
+                self.__setitem__(k,v)
+        else:
+            for i,e in enumerate(wrapped):
+                self.__setitem__(i,e)
+
+    def __setitem__(self, key, value):
+        old_value = self.wrapped[key]
+
+        #self.owning_prop.changed(["K(" + str(key) + ")"], self.owning_instance, old_value, value)
+        self.changed(["K(" + str(key) + ")"], self.owning_instance, old_value, value)
+
+        #value = install(value, self.owning_prop, self.owning_instance, ["K(" + str(key) + ")"])
+        value = install(value, self, self.owning_instance, ["K(" + str(key) + ")"])
+
+        self.wrapped[key] = value
 
     def __getitem__(self, key):
-        return self.the_dict[key]
+        return self.wrapped[key]
 
-class PropertyInstanceWrapper:
-
+    def changed(self, subprops, instance, old_value, new_value):
+        self.owning_prop.changed(subprops, instance, old_value, new_value)
 
 
 class Property:
     def __get__(self, instance, cls):
-        value = instance.__dict__[self.name]
-        if isinstance(value, dict):
-            return PropertyDictWrapper(self, instance, value)
-        else:
-            return value
+        return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
         old_value = instance.__dict__.get(self.name, None)
 
-        instance.propchanged(self, old_value, value)
+        self.changed([], instance, old_value, value)
+
+        value = install(value, self, instance, [self])
 
         instance.__dict__[self.name] = value
 
     def value_init(self, instance):
         value = self.value_producer()
         self.__set__(instance, value)
+
+    def changed(self, subprops, instance, old_value, new_value):
+        instance.__propchange__([self] + subprops, old_value, new_value)
+
+    def __repr__(self):
+        return "P(" + self.name + ")"
 
 
 class PropertyMeta(type):
@@ -50,9 +88,15 @@ class PropertyMeta(type):
         return clsobj
 
 
-class Base(metaclass=PropertyMeta):
-    def propchanged(self, prop, old_value, new_value):
-        print(self,"Changed",prop.name,"from",old_value,"to",new_value)
+class PropertyBase(metaclass=PropertyMeta):
+    def __propchange__(self, prop, old_value, new_value):
+        pass
+
+
+class Base(PropertyBase):
+    def __propchange__(self, prop, old_value, new_value):
+        print("[" + type(self).__qualname__ + "]","Changed",prop,"from",old_value,"to",new_value)
+
 
 class Single(Base):
     val = Property()
@@ -65,9 +109,25 @@ class Alma(Base):
     x = Property()
     y = Property()
 
+class Korte(Alma):
+    pass
+
 
 a = Alma()
-a.x = 20
-a.y = {"egy": Single(50)}
+a.x = {"egy": [Alma()]}
+a.x["egy"][0].x = "korte"
 
-a.y["egy"].val += 100
+
+# a = Alma()
+# a.x = 20
+# a.y = {"egy": Single(50)}
+# a.y["egy"].val += 100
+
+# b = Korte()
+# # b.y = [Single(20),Single(30)]
+# # b.y[0].val += 10
+# # b.y[1].val -= 20
+
+# b.y = Single(30)
+# b.y.val += 1000
+
